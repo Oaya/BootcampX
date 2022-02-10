9a100ff5a4c6dcfd505e9e070bddc6d2d0650f1f
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-02-10 04:07:59 +0000

    parameterized teacher query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,22 +7,24 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const cohortName = process.argv[2] || "JUL02";
+const values = [`${cohortName}`];
+const queryString = `
+SELECT DISTINCT teachers.name AS teacher,   
+   cohorts.name AS cohort
+   FROM assistance_requests
+   JOIN teachers
+   ON assistance_requests.teacher_id = teachers.id
+   JOIN students
+   ON  students.id = assistance_requests.student_id
+   JOIN cohorts
+   ON cohorts.id = students.cohort_id
+   WHERE cohorts.name = $1
+   ORDER BY teacher
+`;
+
 pool
-  .query(
-    `
-    SELECT DISTINCT teachers.name AS teacher,   
-       cohorts.name AS cohort
-       FROM assistance_requests
-       JOIN teachers
-       ON assistance_requests.teacher_id = teachers.id
-       JOIN students
-       ON  students.id = assistance_requests.student_id
-       JOIN cohorts
-       ON cohorts.id = students.cohort_id
-       WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
-       ORDER BY teacher
-`
-  )
+  .query(queryString, values)
   .then((res) => {
     console.log(`connected`);
     res.rows.forEach((row) => {
