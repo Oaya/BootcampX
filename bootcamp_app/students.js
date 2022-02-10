1c1663e130f0ad8c308f483e03d0a82cde5606cd
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-02-10 04:05:20 +0000

    parameterized student query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,18 +6,22 @@ const pool = new Pool({
   host: "localhost",
   database: "bootcampx",
 });
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
-pool
-  .query(
-    `
+const queryString = `
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts
 ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
-  )
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
